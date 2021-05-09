ca788090c2ac7f41359f608dae1a7376560540b1
NikQ <780366011@qq.com>
2021-05-09 20:10:53 +0800

    first ver

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
 import SubMenu from './components/Menu/subMenu'
 import Icon from './components/Icon/icon'
-import Transition from './components/Transition/Transition'
+import Transition from './components/Transition/transition'
 import Button from './components/Button/button'
 
 library.add(fas)
