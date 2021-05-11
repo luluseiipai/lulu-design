038b30c1e208efdba07eb2cb304d56798581ad09
NikQ <780366011@qq.com>
2021-05-11 18:00:42 +0800

    bug fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,3 +18,6 @@ export const decorators = [
 ]
 
 import '../src/styles/index.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
