fab1a028b3e478997e5b073a22fb1d39e0a5aab6
NikQ <780366011@qq.com>
2021-05-14 14:59:26 +0800

    tab 测试

--- src/components/Tab/tab.test.tsx
+++ src/components/Tab/tab.test.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  render,
-  fireEvent,
-  RenderResult,
-  getByText,
-} from '@testing-library/react'
+import { render, fireEvent, RenderResult } from '@testing-library/react'
 // import '@testing-library/jest-dom/extend-expect'
 import { Tab, TabProps } from './Tab'
 import { TabItem } from './TabItem'
