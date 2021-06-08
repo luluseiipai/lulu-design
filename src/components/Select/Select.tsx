68030d4f62ea0f3fb778c16277638f288f0e1a1a
NikQ <780366011@qq.com>
2021-06-08 14:14:20 +0800

    select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -15,7 +15,18 @@ import useDebounce from '../../hooks/useDebounce'
 import useClickOutside from '../../hooks/useClickOutside'
 
 interface SelectProps {
+  /**指定默认选中的条目	 可以是是字符串或者字符串数组*/
   defaultValue: string | string[]
+  /**select input的 name 属性 */
   name: string
+  /**选择框占位符 */
   placeholder: string
+  /**是否禁用 */
+  disabled: boolean
+  /**是否支持多选 */
+  multiple: boolean
+  /**下拉框出现隐藏时触发 */
+  onVisibleChange: (visible: boolean) => void
+  /**选中时触发 */
+  onChange: () => void
 }
